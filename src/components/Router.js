720ab03582eee4fc46d83e6bf9803049f3d87da7
Fluffaro <45957559+Fluffaro@users.noreply.github.com>
2021-12-14 22:17:29 +0800

    5th fix

--- src/components/Router.js
+++ src/components/Router.js
@@ -9,9 +9,9 @@ const Router = () =>{
         <div>
             <BrowserRouter>
                 <Routes>
-                        <Route exact path="/myfirstreact/Home" element={<Home/>}/>
-                        <Route exact path="/" element={<Home/>} />
-                        <Route exact path="/employees" element={<Employee/>}/>
+                        <Route exact path="/myfirstreact/" element={<Home/>}/>
+
+                        <Route exact path="/myfirstreact/employees" element={<Employee/>}/>
                         <Route exact path="/myfirstreact/add" element={<AddEmployee/>}/>
                         <Route exact path="/myfirstreact/employees/edit/:employeeId" element={<AddEmployee/>}/>
                         <Route exact path="*" element={<PageNotFound/>}/>
