07d71a13f22d89c47cecd4eefdc30a53da218835
Fluffaro <45957559+Fluffaro@users.noreply.github.com>
2021-12-14 22:14:43 +0800

    Test

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ const NavBar =() =>{
               <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
                   <a class="nav-link" href="/myfirstreact">Home</a>
-                  <a class="nav-link" href="/myfirstreact/employees">Employees</a>
+                  <a class="nav-link" href="/employees">Employees</a>
                   <a class="nav-link" href="/myfirstreact/add">Add employee</a>
                   <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
                 </div>
